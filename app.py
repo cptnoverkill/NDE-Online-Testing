711476b634f5684a55d01f9220b53aeb1ec404e6
cptnoverkill <33468010+cptnoverkill@users.noreply.github.com>
2024-08-24 13:46:57 -0700

    port added

--- app.py
+++ app.py
@@ -1046,7 +1046,11 @@ def export_exam_result(exam_result_id):
 
 
 
+#if __name__ == '__main__':
+#    with app.app_context():
+#        db.create_all()
+#    app.run(debug=True)
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
